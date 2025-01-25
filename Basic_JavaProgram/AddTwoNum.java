943a427951230e2b08b946dae7da8a0f3e984e33
rahulray27 <business.rahulray27@gmail.com>
2025-01-25 19:17:31 +0530

    New java program

---
+++ Basic_JavaProgram/AddTwoNum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class AddTwoNum {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        int num1 = obj.nextInt();
+        System.out.print("Enter second number : ");
+        int num2 = obj.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Sum : " + sum);
+    }
+}
